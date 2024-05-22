553bf60730f18b77edde93c1a76e152cfc247813
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-05-22 02:30:03 +0100

    modified:   src/views/protected/AdminModule/AppUsers.vue

--- src/views/protected/AdminModule/AppUsers.vue
+++ src/views/protected/AdminModule/AppUsers.vue
@@ -472,7 +472,12 @@ const birthMonthOptions = computed<string[]>(() => [
               <TableHead>Fullname</TableHead>
               <TableHead>Birthday</TableHead>
               <TableHead> Gender</TableHead>
-              <TableHead>Wallet Balance</TableHead>
+              <TableHead>
+                <div class="flex items-center">
+                  Wallet Balance
+                  <Icon icon="fluent:chevron-up-down-20-regular" class="ml-1" />
+                </div>
+              </TableHead>
               <TableHead>Status</TableHead>
               <TableHead></TableHead>
             </TableRow>
@@ -486,13 +491,13 @@ const birthMonthOptions = computed<string[]>(() => [
               <TableCell class="text-xs md:text-sm lg:text-sm">{{ user.dob }} </TableCell>
               <TableCell class="text-xs md:text-sm lg:text-sm">{{ user.gender }} </TableCell>
               <TableCell class="text-xs md:text-sm lg:text-sm">{{ user.balance }}</TableCell>
-              <TableCell class="flex">
+              <TableCell class="flex items-center">
                 <!-- Render multiple status icons based on user's status array -->
                 <template v-for="status in user.status" :key="status">
                   <img
                     :src="getStatusIconUrl(status)"
                     :alt="status"
-                    class="h-[20px] lg:h-[20px] w-auto mr-1"
+                    class="h-[30px] w-auto mr-1 my-1"
                   />
                 </template>
               </TableCell>
