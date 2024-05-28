7f317b84ac103bfb9d10f7d9829a316a2e478b49
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-05-28 22:28:02 +0100

    this page user activity log

---
+++ src/stores/activity-log/activity-log.ts
@@ -0,0 +1,72 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+interface UserExtras {
+  email: string;
+  userName: string;
+}
+
+interface User {
+  id: string;
+  type: string;
+  extras: UserExtras;
+}
+
+interface Resource {
+  ip: string;
+  timestamp: number;
+  user_agent: string;
+}
+
+interface ActivityLogItem {
+  id: string;
+  timestamp: string;
+  user: User;
+  action: string;
+  resource: Resource;
+  metadata: Record<string, unknown>;
+  status: string;
+  description: string;
+}
+
+interface ActivityLogState {
+  logs: ActivityLogItem[];
+  loading: boolean;
+  error: string | null;
+}
+
+export const useActivityLogStore = defineStore('activityLog', {
+  state: (): ActivityLogState => ({
+    logs: [],
+    loading: false,
+    error: null,
+  }),
+  actions: {
+    async fetchActivityLogs(userId: string) {
+      this.loading = true;
+      this.error = null;
+
+      const token = sessionStorage.getItem('token') || ''
+      
+      try {
+        const response = await axios.get(`https://api.staging.weeshr.com/api/v1/admin/accounts/user/${userId}/logs`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+        );
+        
+        if (response.data.code === 200) {
+          this.logs = response.data.data.data;
+        } else {
+          this.error = response.data.message;
+        }
+      } catch (error) {
+        this.error = error instanceof Error ? error.message : 'Unknown error';
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
