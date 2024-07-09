c95468247186944ac9faf0ac3326f74d76ae7971
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-07-09 03:38:41 +0100

    Implement the dashboard analytics

--- src/views/protected/AdminModule/AdminDashboard.vue
+++ src/views/protected/AdminModule/AdminDashboard.vue
@@ -5,7 +5,7 @@ import DashboardFooter from '@/components/DashboardFooter.vue';
 import { Card, CardContent } from '@/components/ui/card';
 import { useUserhubStore } from '@/stores/userhub-details/userhub-details';
 import usersTable from '@/components/usersTable.vue';
-import { ability } from '@/lib/ability';g
+import { ability } from '@/lib/ability';
 import { defineAbilities } from '@/lib/ability';
 import { useWeeshStore } from '@/stores/weeshes/weeshes-count';
 import { useDashboardAnalyticsStore } from '@/stores/dashboard-analytics/dashboard-analytics';
