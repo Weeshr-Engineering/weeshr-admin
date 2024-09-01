eea71905a7c22029b3d1549de46bb0977f2a8e43
ufy <ufuomaitwa@gmail.com>
2024-09-01 16:50:17 +0100

    Bank details

--- src/views/protected/AdminModule/BankPage.vue
+++ src/views/protected/AdminModule/BankPage.vue
@@ -12,7 +12,9 @@
             <div class="weeshr-icon2 rounded-[7px]">
               <Icon icon="solar:book-bold" width="24px" height="24px" color="#020721" />
             </div>
-            <p class="text-2xl md:text-xl xl:text-3xl font-medium text-[#020721] absolute bottom-2 left-5">
+            <p
+              class="text-2xl md:text-xl xl:text-3xl font-medium text-[#020721] absolute bottom-2 left-5"
+            >
               ₦ 78,351,823.74
             </p>
           </CardContent>
@@ -34,7 +36,9 @@
               />
             </div>
 
-            <p class="text-2xl md:text-xl xl:text-3xl font-medium text-[#ffffff] absolute bottom-2 left-5">
+            <p
+              class="text-2xl md:text-xl xl:text-3xl font-medium text-[#ffffff] absolute bottom-2 left-5"
+            >
               ₦ 176,405,287.78
             </p>
           </CardContent>
@@ -51,7 +55,9 @@
               <Icon icon="heroicons:bars-3" width="24px" height="24px" color="#EE9F39" />
             </div>
 
-            <p class="text-2xl md:text-xl xl:text-3xl font-medium text-[#ffffff] absolute bottom-2 left-5">
+            <p
+              class="text-2xl md:text-xl xl:text-3xl font-medium text-[#ffffff] absolute bottom-2 left-5"
+            >
               ₦ 32,125.00
             </p>
           </CardContent>
@@ -73,7 +79,9 @@
               />
             </div>
 
-            <p class="text-2xl md:text-xl xl:text-3xl font-medium text-[#ffffff] absolute bottom-2 left-5">
+            <p
+              class="text-2xl md:text-xl xl:text-3xl font-medium text-[#ffffff] absolute bottom-2 left-5"
+            >
               ₦ 98,021,339.04
             </p>
           </CardContent>
@@ -82,24 +90,59 @@
     </div>
 
     <!-- Account -->
-    <div v-if='readRole' class="w-full px-6 py-4 flex flex-col gap-8 md:gap-0 md:flex-row items-start md:items-center justify-between bg-[#E4E6F5] rounded-xl mt-4 md:mt-6">
-      <div class='flex items-center gap-2'>
-        <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M14.7906 23.2841C15.1111 23.3587 15.1405 23.7785 14.8283 23.8826L12.985 24.4892C8.3533 25.9826 5.91497 24.7342 4.40997 20.1026L2.91664 15.4942C1.4233 10.8626 2.65997 8.41255 7.29164 6.91922L8.0723 6.66069C8.47517 6.52727 8.86472 6.9304 8.74591 7.33781C8.66197 7.62565 8.58233 7.92946 8.50497 8.24922L7.36164 13.1376C6.0783 18.6326 7.95664 21.6659 13.4516 22.9726L14.7906 23.2841Z" fill="#020721"/>
-          <path d="M20.0317 3.7446L18.0833 3.2896C14.1867 2.36794 11.865 3.12627 10.5 5.9496C10.15 6.66127 9.87001 7.5246 9.63667 8.51627L8.49334 13.4046C7.35001 18.2813 8.85501 20.6846 13.72 21.8396L15.68 22.3063C16.3567 22.4696 16.9867 22.5746 17.57 22.6213C21.21 22.9713 23.1467 21.2679 24.1267 17.0563L25.27 12.1796C26.4133 7.30294 24.92 4.88794 20.0317 3.7446ZM17.8383 15.5513C17.7333 15.9479 17.3833 16.2046 16.9867 16.2046C16.9167 16.2046 16.8467 16.1929 16.765 16.1813L13.37 15.3179C12.9033 15.2013 12.6233 14.7229 12.74 14.2563C12.8567 13.7896 13.335 13.5096 13.8017 13.6263L17.1967 14.4896C17.675 14.6063 17.955 15.0846 17.8383 15.5513ZM21.2567 11.6079C21.1517 12.0046 20.8017 12.2613 20.405 12.2613C20.335 12.2613 20.265 12.2496 20.1833 12.2379L14.525 10.8029C14.0583 10.6863 13.7783 10.2079 13.895 9.74127C14.0117 9.2746 14.49 8.9946 14.9567 9.11127L20.615 10.5463C21.0933 10.6513 21.3733 11.1296 21.2567 11.6079Z" fill="#020721"/>
+    <div
+      v-if="readRole"
+      class="w-full px-6 py-4 flex flex-col gap-8 md:gap-0 md:flex-row items-start md:items-center justify-between bg-[#E4E6F5] rounded-xl mt-4 md:mt-6"
+    >
+      <div class="flex items-center gap-2">
+        <svg
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M14.7906 23.2841C15.1111 23.3587 15.1405 23.7785 14.8283 23.8826L12.985 24.4892C8.3533 25.9826 5.91497 24.7342 4.40997 20.1026L2.91664 15.4942C1.4233 10.8626 2.65997 8.41255 7.29164 6.91922L8.0723 6.66069C8.47517 6.52727 8.86472 6.9304 8.74591 7.33781C8.66197 7.62565 8.58233 7.92946 8.50497 8.24922L7.36164 13.1376C6.0783 18.6326 7.95664 21.6659 13.4516 22.9726L14.7906 23.2841Z"
+            fill="#020721"
+          />
+          <path
+            d="M20.0317 3.7446L18.0833 3.2896C14.1867 2.36794 11.865 3.12627 10.5 5.9496C10.15 6.66127 9.87001 7.5246 9.63667 8.51627L8.49334 13.4046C7.35001 18.2813 8.85501 20.6846 13.72 21.8396L15.68 22.3063C16.3567 22.4696 16.9867 22.5746 17.57 22.6213C21.21 22.9713 23.1467 21.2679 24.1267 17.0563L25.27 12.1796C26.4133 7.30294 24.92 4.88794 20.0317 3.7446ZM17.8383 15.5513C17.7333 15.9479 17.3833 16.2046 16.9867 16.2046C16.9167 16.2046 16.8467 16.1929 16.765 16.1813L13.37 15.3179C12.9033 15.2013 12.6233 14.7229 12.74 14.2563C12.8567 13.7896 13.335 13.5096 13.8017 13.6263L17.1967 14.4896C17.675 14.6063 17.955 15.0846 17.8383 15.5513ZM21.2567 11.6079C21.1517 12.0046 20.8017 12.2613 20.405 12.2613C20.335 12.2613 20.265 12.2496 20.1833 12.2379L14.525 10.8029C14.0583 10.6863 13.7783 10.2079 13.895 9.74127C14.0117 9.2746 14.49 8.9946 14.9567 9.11127L20.615 10.5463C21.0933 10.6513 21.3733 11.1296 21.2567 11.6079Z"
+            fill="#020721"
+          />
         </svg>
         <p>Cash Request</p>
       </div>
-      <div class='flex items-center gap-2 text-nowrap'>
-        <p class='text-lg md:text-2xl'>
-          ₦ 5,190,500.00
-        </p>
-        <RouterLink to="/bank/cash-request"><svg width="35" height="35" class='h-6 w-6 md:w-auto md:h-auto' viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M15 36.6673H25C33.3334 36.6673 36.6667 33.334 36.6667 25.0007V15.0007C36.6667 6.66732 33.3334 3.33398 25 3.33398H15C6.66671 3.33398 3.33337 6.66732 3.33337 15.0007V25.0007C3.33337 33.334 6.66671 36.6673 15 36.6673Z" stroke="#020721" stroke-opacity="0.6" stroke-width="3.0625" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M17.9 25.8839L23.7667 20.0005L17.9 14.1172" stroke="#020721" stroke-opacity="0.6" stroke-width="3.0625" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg></RouterLink>
+      <div class="flex items-center gap-2 text-nowrap">
+        <p class="text-lg md:text-2xl">₦ 5,190,500.00</p>
+        <RouterLink to="/bank/cash-request"
+          ><svg
+            width="35"
+            height="35"
+            class="h-6 w-6 md:w-auto md:h-auto"
+            viewBox="0 0 40 40"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15 36.6673H25C33.3334 36.6673 36.6667 33.334 36.6667 25.0007V15.0007C36.6667 6.66732 33.3334 3.33398 25 3.33398H15C6.66671 3.33398 3.33337 6.66732 3.33337 15.0007V25.0007C3.33337 33.334 6.66671 36.6673 15 36.6673Z"
+              stroke="#020721"
+              stroke-opacity="0.6"
+              stroke-width="3.0625"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M17.9 25.8839L23.7667 20.0005L17.9 14.1172"
+              stroke="#020721"
+              stroke-opacity="0.6"
+              stroke-width="3.0625"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            /></svg
+        ></RouterLink>
       </div>
-     </div>
+    </div>
 
     <Card
       class="container px-4 pt-6 pb-10 mx-auto sm:px-6 lg:px-8 bg-[#FFFFFF] rounded-2xl mt-14 mb-4"
@@ -154,8 +197,12 @@
               <TableCell class="text-xs md:text-sm lg:text-sm">{{ user.id }}</TableCell>
               <TableCell class="text-xs md:text-sm lg:text-sm">{{ user.weeshes }} </TableCell>
               <TableCell class="text-xs md:text-sm lg:text-sm">{{ user.type }} </TableCell>
-              <TableCell class="text-xs md:text-sm lg:text-sm text-nowrap">{{ user.date }} </TableCell>
-              <TableCell class="text-xs md:text-sm lg:text-sm text-nowrap">{{ user.amount }} </TableCell>
+              <TableCell class="text-xs md:text-sm lg:text-sm text-nowrap"
+                >{{ user.date }}
+              </TableCell>
+              <TableCell class="text-xs md:text-sm lg:text-sm text-nowrap"
+                >{{ user.amount }}
+              </TableCell>
               <TableCell>
                 <div
                   :class="statusBg(user.status)"
@@ -206,18 +253,14 @@
 
 <script setup lang="ts">
 import Search from '@/components/UseSearch.vue'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import MainNav from '@/components/MainNav.vue'
 import DashboardFooter from '@/components/DashboardFooter.vue'
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Icon } from '@iconify/vue'
-import { ability, defineAbilities, verifyAbilities } from '@/lib/ability';
-
-defineAbilities()
-const readRole = ability.can('read', 'wallet-payouts');
-
-
+import { ability, defineAbilities } from '@/lib/ability'
+import { getBank } from '@/composables/bankDetails'
 import {
   Table,
   TableRow,
@@ -226,6 +269,15 @@ import {
   TableCell,
   TableHead
 } from '@/components/ui/table'
+
+defineAbilities()
+const readRole = ability.can('read', 'wallet-payouts', 'read-weeshr-bank')
+
+const { getBankDetails, balance } = getBank()
+
+onMounted(() => {
+  getBankDetails()
+})
 
 // Define a ref to hold the users data
 // const users = ref([]);
